03fd0fceb0946513720f82a3678842cab157b120
kamomil <dafna3@gmail.com>
2017-04-16 23:55:26 +0300

    printing kernel data and pointer memory zone , also trying to mmap a physical reserved memory to userspace

---
+++ lab2_platform_dt.c
@@ -0,0 +1,227 @@
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/device.h>
+#include <linux/of.h>
+#include "lab_miscdev.h"
+#include <linux/interrupt.h>
+#include <linux/of_address.h>
+#include <linux/mm.h>
+#include <linux/pagemap.h>
+#include <linux/vmalloc.h>
+#include "print_things.h"
+
+#define TX_IDX 0
+#define RX_IDX 1
+
+
+struct resource* resources [2];
+const char* resources_types[2]  = {"tx","rx"};
+const char* resources_types_dev[2]  = {"lab2_interuppts_tx","lab2_interuppts_rx"};
+
+
+int mycdrv_mymmap (struct file *file, struct vm_area_struct *vma)
+{
+  //int remap_pfn_range(struct vm_area_struct *, unsigned long addr,unsigned long pfn, unsigned long size, pgprot_t);
+  unsigned long mmap_size;
+  phys_addr_t ramdisk =  0x0000000086000000;
+  //unsigned long  range_size = 300000;
+
+  if(!file && !vma){
+    dev_info(my_dev,"MMAP internaly called\n");
+    return 0;
+  }    
+  mmap_size = vma->vm_end - vma->vm_start;
+
+  print_virtual_reg(__va(ramdisk));
+
+  dev_info(my_dev,"MMAP: PAGE_SIZE %lu PAGE_SHIFT %d ramdisk (virtual) = %p phisical (0x%llx), mmap_size = %ld\n", PAGE_SIZE,PAGE_SHIFT, __va(ramdisk),ramdisk, mmap_size);
+  
+  dev_info(my_dev,"MMAP: before: vma = %p, vma->vm_start = %p, __pa(ramdisk) = 0x%llx, mmap_size = %ld, vma->vm_page_prot = 0x%llx\n",vma, (void *)vma->vm_start, __pa(ramdisk), mmap_size, vma->vm_page_prot);
+
+ 
+  if(remap_pfn_range(vma, vma->vm_start, __va(ramdisk), mmap_size, vma->vm_page_prot)){
+    dev_warn(my_dev,"remap_pfn_range failed\n");
+    return -EAGAIN;
+  }
+  vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
+
+  dev_info(my_dev,"MMAP: after: vma = %p, vma->vm_start = %p,  vma->vm_page_prot = 0x%llx\n",vma, (void *)vma->vm_start, vma->vm_page_prot);
+    
+  return 0;
+}
+
+static void reserved_mem_range(void){
+
+  struct device_node *parent, *node;
+    
+  parent = of_find_node_by_path("/reserved-memory");
+  if (!parent)
+    return;
+    
+  for_each_child_of_node(parent, node) {
+    uint64_t range_addr, range_size, range_end;
+    const __be32 *addrp;
+    const char *label;
+    
+    addrp = of_get_address(node, 0, &range_size, NULL);
+    
+    range_addr = of_read_number(addrp, 2);
+    range_end = range_addr + range_size;
+    
+    label = of_get_property(node, "rmtfs", NULL);
+
+    pr_info("\nreserved_mem_range: addrp = %p\n",addrp);
+    pr_info("reserved_mem_range: range_addr = %p\n",(void*)range_addr);
+    pr_info("reserved_mem_range: range_addr = %llx\n",range_addr);
+
+    pr_info("reserved_mem_range: range_size = %p\n",(void*)range_size);
+    pr_info("reserved_mem_range: range_size = %llx\n",range_size);
+    
+    /* PRD ranges need a label */
+    if (!label)
+      continue;
+
+    return;
+  }
+}
+
+static int my_probe(struct platform_device *pdev)
+{
+  struct device_node* dn;
+  struct  property *p = NULL;
+  void *virt_addr_ptr = NULL;
+  struct page **my_page = NULL;
+  unsigned long phys_addr_ptr =  0x0000000086000000;
+  
+  pr_info("MY_PROBE: pdev = %p\n",pdev);
+  memset(resources,0,sizeof(resources));
+
+  /* platform device is defined in linux/platform_device.h */
+  /* struct device_node is defined in include/linux/of.h */
+  /* struct device  is defined in include/device.h*/
+
+   pr_info("MY_PROBE: pdev->name = %s\n",pdev->name);
+   pr_info("MY_PROBE: pdev->id = %d\n",pdev->id);
+   pr_info("MY_PROBE: pdev->id_auto = %d\n",pdev->id_auto);
+
+   pr_info("MY_PROBE: pdev->num_resources = %d\n",pdev->num_resources);
+   pr_info("MY_PROBE: pdev->*driver_override = %s\n",pdev->driver_override); /* Driver name to force a match */
+
+   dn = pdev->dev.of_node;
+   pr_info("MY_PROBE: pdev->dev.of_node = %p\n",dn); 
+
+   pr_info("MY_PROBE: dn->name = %p\n",dn->name);
+   pr_info("MY_PROBE: dn->type = %p\n",dn->type);
+
+   for(p = dn->properties;p != NULL;p = p->next){
+     pr_info("/nMY_PROBE: p->name = %s\n",p->name);
+     pr_info("MY_PROBE: p->length = %d\n",p->length);
+     pr_info("MY_PROBE: p->value = %p\n",p->value);
+     pr_info("MY_PROBE: p->value = %s\n",(char*)(p->value));
+     pr_info("MY_PROBE: p->_flags = %lx\n",p->_flags);
+     pr_info("MY_PROBE: p->unique_id = %x\n",p->unique_id);
+	       
+   }
+   reserved_mem_range();
+
+   //my_page = (struct page**)kmalloc(sizeof (*my_page), GFP_KERNEL);
+   //*my_page = phys_to_page(phys_addr_ptr);
+   //virt_addr_ptr = vmap(my_page, 1, VM_MAP, PAGE_KERNEL);
+   //pr_info("/nMY_PROBE: p->unique_id = %x\n",p->unique_id);
+   
+   /* 26         struct device   dev;*/
+   /* 28         struct resource *resource;*/
+   
+   /* 30         const struct platform_device_id *id_entry;*/
+   
+   /* MFD cell pointer */
+   /*        struct mfd_cell *mfd_cell;*/
+   
+   /* arch specific additions */
+   /*         struct pdev_archdata    archdata;*/
+   
+  
+  return 0;
+}
+
+static int my_remove(struct platform_device *pdev)
+{
+  
+  pr_info("MY_REMOVE: pdev = %p",pdev);
+  return 0;
+}
+
+
+static const struct of_device_id lab2_of_match[] = {
+	{ .compatible = "stam,devdev" },
+	{}
+};
+MODULE_DEVICE_TABLE(of, lab2_of_match);
+
+static struct platform_driver dafna_driver = {
+  //struct device_driver driver;
+  .driver= {
+    .name = "lab2_platform_dt",//any device identifying itself as "lab2_platform_dt" will be bound to this driver
+    
+    .of_match_table = lab2_of_match,
+  },
+  .probe = my_probe,
+  .remove = my_remove,
+  //.id_table = pwm_id_table,
+};
+
+/*
+tx irq: 208 
+rx irq: 209
+ */
+static int __init interrupt_init(void)
+{
+  int err = platform_driver_register(&dafna_driver);
+  if (err){
+    pr_err("lab2 in init: platform_driver_register failed\n");
+    return err;
+  }
+  
+  ramdisk1 = kmalloc(ramdisk_size, GFP_KERNEL);
+  if(!ramdisk1){
+      pr_err("lab2 interupts (in init): could not allocate ramdisk\n");
+      return -1;
+  }
+  
+  if (misc_register(&my_misc_device)) {
+    pr_err("Couldn't register device misc, "
+	   "%d.\n", my_misc_device.minor);
+    kfree(ramdisk1);
+    platform_driver_unregister(&dafna_driver);
+    return -EBUSY;
+  }
+  my_dev = my_misc_device.this_device;
+  dev_info(my_dev, "interrupt_init: Succeeded in registering device %s\n", MYDEV_NAME);
+
+  return 0;
+}
+
+static void __exit  interrupt_exit(void)
+{
+  dev_info(my_dev, "Unregistering Device\n");
+  platform_driver_unregister(&dafna_driver);
+  misc_deregister(&my_misc_device);
+  kfree(ramdisk1);
+}
+
+static const struct file_operations mycdrv_fops = {
+  .owner = THIS_MODULE,
+  .read = mycdrv_generic_read,
+  .write = mycdrv_generic_write,
+  .open = mycdrv_generic_open,
+  .release = mycdrv_generic_release,
+    .mmap = mycdrv_mymmap
+};
+
+module_init(interrupt_init);
+module_exit(interrupt_exit);
+
+MODULE_AUTHOR("Dafna");
+MODULE_DESCRIPTION("dummy stam");
+MODULE_LICENSE("GPL v2");
+//MODULE_ALIAS("of:N*T*Cqcom,wcnss-wlan*");
---
+++ print_things.h
@@ -0,0 +1,91 @@
+#include <linux/ioctl.h>
+#include <asm/io.h>
+#include <linux/memblock.h>
+#include <asm/fixmap.h>
+#include <asm/memory.h>
+
+//#include <linux/mm.h>
+//#include <linux/bootmem.h>
+
+//#include <asm-arm64/page.h>
+//#include <asm/pgtable_types.h>
+//#include <asm/pgtable.h>
+
+/*
+
+vmalloc : 0xffffff8000000000 - 0xffffffbdbfff0000   (   246 )
+vmemmap : 0xffffffbdc0000000 - 0xffffffbfc0000000   (     8 )
+          0xffffffbdc0000000 - 0xffffffbdc1000000   (    16 )
+fixed   : 0xffffffbffa7fd000 - 0xffffffbffac00000   (  4108 )
+PCI I/O : 0xffffffbffae00000 - 0xffffffbffbe00000   (    16 )
+modules : 0xffffffbffc000000 - 0xffffffc000000000   (    64 )
+memory  : 0xffffffc000000000 - 0xffffffc040000000   (  1024 )
+.init : 0xffffffc000e49000 - 0xffffffc000f43000   (  1000 )
+.text : 0xffffffc000080000 - 0xffffffc000e483e4   ( 14113 )
+
+ */
+/*
+		  MLG(VMEMMAP_START,
+		      VMEMMAP_START + VMEMMAP_SIZE),
+		  MLM((unsigned long)virt_to_page(PAGE_OFFSET),
+		      (unsigned long)virt_to_page(high_memory)),
+
+ */
+int print_all_kinds(void){
+  //phys_addr_t addr1;
+  //phys_addr_t addr2;
+  
+  printk( KERN_INFO "PAGE_OFFSET = 0x%lx\n",PAGE_OFFSET);//#define __phys_to_virt(x)       ((unsigned long)((x) - PHYS_OFFSET) | PAGE_OFFSET)
+  printk( KERN_INFO "PAGE_SHIFT = %d\n",PAGE_SHIFT);
+  printk( KERN_INFO "high_memory = 0x%p\n",high_memory);
+  printk( KERN_INFO "(unsigned long)virt_to_page(PAGE_OFFSET) = 0x%lx\n",(unsigned long)virt_to_page(PAGE_OFFSET));
+  printk( KERN_INFO "(unsigned long)virt_to_page(high_memory) = 0x%lx\n",(unsigned long)virt_to_page(high_memory));
+  printk( KERN_INFO "PHYS_OFFSET = 0x%llx\n",PHYS_OFFSET);
+  //printk( KERN_INFO "TEXT_OFFSET = 0x%llx\n",TEXT_OFFSET); //undeclared
+  
+  //printk( KERN_INFO "kimage_vaddr = 0x%lx\n",kimage_vaddr);
+  //  addr2 = get_allocated_memblock_reserved_regions_info(&addr1);
+  //printk( KERN_INFO "get_allocated_memblock_reserved_regions_info = 0x%llx, 0x%llx",addr1,addr2);
+  return 0;
+}
+int print_mem_regs(void){
+
+  printk( KERN_INFO "vmalloc: starts: 0x%lx ends 0x%lx\n",VMALLOC_START,VMALLOC_END);
+  printk( KERN_INFO "vmemmap: starts: 0x%lx ends 0x%lx\n",VMEMMAP_START,VMEMMAP_START+VMEMMAP_SIZE);
+  printk( KERN_INFO "fixed:   starts: 0x%lx ends 0x%lx\n",FIXADDR_START,FIXADDR_TOP);
+  printk( KERN_INFO "PCI I/O: starts: 0x%lx ends 0x%lx\n",PCI_IO_START, PCI_IO_END);
+  printk( KERN_INFO "modules: starts: 0x%lx ends 0x%lx\n",MODULES_VADDR, MODULES_END);
+  printk( KERN_INFO "memory:  starts: 0x%lx ends 0x%lx\n",PAGE_OFFSET,(unsigned long)high_memory);
+    
+  printk(KERN_INFO "\n");
+  return 0;
+}
+
+int print_virtual_reg(void* p){
+
+  printk( KERN_INFO "p is %p \n",p);
+
+  if(p>=VMALLOC_START && p< VMALLOC_END){
+    printk( KERN_INFO "p is in the vmalloc region, starts: 0x%lx ends 0x%lx\n",VMALLOC_START,VMALLOC_END);
+  }
+  if(p >= VMEMMAP_START && p < VMEMMAP_START + VMEMMAP_SIZE){
+    printk( KERN_INFO "p is in the vmemmap region, starts: 0x%lx ends 0x%lx\n",VMEMMAP_START,VMEMMAP_START+VMEMMAP_SIZE);
+  }
+  if(p >= FIXADDR_START && p < FIXADDR_TOP){
+    printk( KERN_INFO "p is in the fixed region, starts: 0x%lx ends 0x%lx\n",FIXADDR_START,FIXADDR_TOP);
+  }
+
+  if(p >= PCI_IO_START && p < PCI_IO_END){
+    printk( KERN_INFO "p is in the PCI I/O region, starts: 0x%lx ends 0x%lx\n",PCI_IO_START, PCI_IO_END);
+  }
+
+  if(p >= MODULES_VADDR && p < MODULES_END){
+    printk( KERN_INFO "p is in the modules region, starts: 0x%lx ends 0x%lx\n",MODULES_VADDR, MODULES_END);
+  }
+
+  if(p >= PAGE_OFFSET && p < (unsigned long)high_memory){
+    printk( KERN_INFO "p is in the memory, starts: 0x%lx ends 0x%lx\n",PAGE_OFFSET,(unsigned long)high_memory);
+  }
+  printk(KERN_INFO "\n");
+  return 0;
+}
